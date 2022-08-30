60a2247152d38f55e615e51d9cabd6b936b2255e
Daniel <delatorrechapell@gmail.com>
2022-08-30 17:14:10 -0500

    Grid creation: complete (16x16)
    Added change background color of squares on hover

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-let container = document.querySelector('#container');
-let squares = document.createElement('div');
-squares.classList.add('squares');
-container.appendChild(squares);
+function createGrid (size) {
+  let grid = document.querySelector('#grid');
+  for (let j = size; j > 0; j--) {
+    grid.gridRow += 0;
+    for (let i = size; i > 0; i--){
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        grid.appendChild(cell);
+        cell.addEventListener('mouseover', function handleMouseOver() {
+          cell.style.background = 'blue';
+      });
+        grid.gridRow++;
+    }
+  }
+}
+
+let size = 16;
+let button = document.querySelector('button');
+button.addEventListener('click', function handleClick() {
+  prompt("Please enter the size of the squared grid", size)
+});
+
+createGrid(size);
