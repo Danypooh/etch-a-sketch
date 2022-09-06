617abe405a158da225796404ac9488c00a97d514
Daniel <delatorrechapell@gmail.com>
2022-09-06 18:53:57 -0500

    Added clear button with function to clear grid

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 let size;
-let button = document.querySelector('button');
+let button = document.querySelector('#grid-size');
 button.addEventListener('click', function handleClick() {
   size = window.prompt("Please enter the number of squares per side for the new grid");
   createGrid(size);
 });
 
-let grid = document.querySelector('#grid');
-//createGrid(size);
+let clear = document.querySelector('#clear');
+clear.addEventListener('click', function handleClick() {
+  try {
+    clearGrid();
+  }
+  catch (error) {
+    console.log("There's nothing to clear");
+  }
+}); 
 
 function createGrid (size = 16) {  //CSSgrid
   let grid = document.querySelector('#grid');
@@ -20,6 +27,16 @@ function createGrid (size = 16) {  //CSSgrid
         cell.addEventListener('mouseover', function handleMouseOver() {
           cell.style.background = 'blue';
       });
+    }
+  }
+}
+
+function clearGrid () {
+  let grid = document.querySelector('#grid');
+  for (let j = size; j > 0; j--) {
+    for (let i = size; i > 0; i--){
+        let cell = document.querySelector('.cell');
+        grid.removeChild(cell);
     }
   }
 }
