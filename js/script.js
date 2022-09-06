1cd3fb4606f8b3304ed71863235db8d937085b5a
Daniel <delatorrechapell@gmail.com>
2022-09-06 18:23:01 -0500

    Centered grid & made cells dynamic
    cells now occupy space relative to the size of the grid
    entered by the user

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
+let size;
+let button = document.querySelector('button');
+button.addEventListener('click', function handleClick() {
+  size = window.prompt("Please enter the number of squares per side for the new grid");
+  createGrid(size);
+});
+
+let grid = document.querySelector('#grid');
+//createGrid(size);
+
 function createGrid (size = 16) {  //CSSgrid
   let grid = document.querySelector('#grid');
-  grid.style.gridTemplateColumns = `repeat(${size}, 1px)`
-  grid.style.gridTemplateRows = `repeat(${size}, 1px)`
+  grid.style.gridTemplateColumns = `repeat(${size}, auto)`
+  grid.style.gridTemplateRows = `repeat(${size}, auto)`
   for (let j = size; j > 0; j--) {
     for (let i = size; i > 0; i--){
         let cell = document.createElement('div');
@@ -13,12 +23,3 @@ function createGrid (size = 16) {  //CSSgrid
     }
   }
 }
-
-let size;
-let button = document.querySelector('button');
-button.addEventListener('click', function handleClick() {
-  size = window.prompt("Please enter the number of squares per side for the new grid");
-  createGrid(size);
-});
-
-//createGrid(size);
